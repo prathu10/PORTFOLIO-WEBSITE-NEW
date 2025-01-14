7104279fedb0e3f34b78899251e301effbefa2a3
Prathu <prathamesh4402@gmail.com>
2025-01-14 01:54:43 -0700

    new skill added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',  // This is important for Netlify
-  },
-});
+  plugins: [react()],
+})
